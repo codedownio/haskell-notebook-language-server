d7750ab917022253d779f0f80cb66428ae5cf89e
Tom McLaughlin <tom@codedown.io>
2022-02-02 22:00:56 -0800

    Projecting imports looks good

--- test/Main.hs
+++ test/Main.hs
@@ -20,6 +20,14 @@ spec = do
     transformPosition () sifter (Position 0 0) `shouldBe` (Just (Position 1 0))
     transformPosition () sifter (Position 1 0) `shouldBe` (Just (Position 0 0))
 
+  it "projects and transforms a two imports" $ do
+    let (ls, sifter@(FrontSifter indices)) = project () ["putStrLn 42", "import Bar", "foo = 42", "import Foo", "bar = 24"]
+    ls `shouldBe` ["import Bar", "import Foo", "putStrLn 42", "foo = 42", "bar = 24"]
+    indices `shouldBe` [1, 3]
+
+    transformPosition () sifter (Position 0 0) `shouldBe` (Just (Position 2 0))
+    transformPosition () sifter (Position 1 0) `shouldBe` (Just (Position 0 0))
+
 
 main :: IO ()
 main = runSandwichWithCommandLineArgs defaultOptions spec

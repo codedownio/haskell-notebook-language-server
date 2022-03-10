5efcef24ba7b84a0776cc28c2ff3e4f1cc60dc28
Tom McLaughlin <tom@codedown.io>
2022-03-09 22:24:20 -0800

    Builds!

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 {
-  haskellNix ? import (builtins.fetchTarball "https://github.com/codedownio/haskell.nix/archive/11321af2b62e67a11ddfc8250617ed32e92aa631.tar.gz") {}
+  haskellNix ? import (builtins.fetchTarball "https://github.com/codedownio/haskell.nix/archive/5867bbb4aba60d7bcc1d4291e23325a4c38efdb5.tar.gz") {}
 
 , nixpkgsArgs ? haskellNix.nixpkgsArgs
 
@@ -35,4 +35,23 @@ haskell-nix.project {
   stack-sha256 = "149p2nlb7di9zg27z8gv0k4kdjxbxkf7p5d8grijk0j1d9fln0zr";
   materialized = ./materialized;
   inherit checkMaterialization;
+
+  modules = [{
+    # https://github.com/input-output-hk/haskell.nix/issues/1177#issuecomment-891568396
+    nonReinstallablePkgs = [
+      "rts" "ghc-heap" "ghc-prim" "integer-gmp" "integer-simple" "base"
+      "deepseq" "array" "ghc-boot-th" "pretty" "template-haskell"
+      # ghcjs custom packages
+      "ghcjs-prim" "ghcjs-th"
+      "ghc-bignum" "exceptions" "stm"
+      "ghc-boot"
+      "ghc" "Cabal" "Win32" "array" "binary" "bytestring" "containers"
+      "directory" "filepath" "ghc-boot" "ghc-compact" "ghc-prim"
+      # "ghci" "haskeline"
+      "hpc"
+      "mtl" "parsec" "process" "text" "time" "transformers"
+      "unix" "xhtml" "terminfo"
+    ];
+
+  }];
 }

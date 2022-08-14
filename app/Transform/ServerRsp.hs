02e48a23368499333a6a9ae023eff50991637360
Tom McLaughlin <tom@codedown.io>
2022-08-14 02:41:27 -0700

    Untransform hover

--- app/Transform/ServerRsp.hs
+++ app/Transform/ServerRsp.hs
@@ -7,6 +7,7 @@ module Transform.ServerRsp where
 import Control.Lens
 import Control.Monad.Logger
 import Data.String.Interpolate
+import Language.LSP.Notebook (HaskellNotebookTransformer)
 import Language.LSP.Transformer
 import Language.LSP.Types
 import Language.LSP.Types.Lens as Lens
@@ -25,17 +26,19 @@ transformServerRsp meth initialParams msg = do
       return $ set result (Right p') msg
 
 transformServerRsp' :: (TransformerMonad n) => ServerRspMethod m -> MessageParams m -> ResponseResult m -> n (ResponseResult m)
-transformServerRsp' STextDocumentDocumentHighlight initialParams result = whenNotebookResult initialParams result $ \uri -> do
-  lookupTransformer uri >>= \case
-    Nothing -> do
-      logWarnN [i|Couldn't find expected transformer for uri #{uri}|]
-      return result
-    Just tx -> do
-      let transform :: DocumentHighlight -> DocumentHighlight
-          transform x = x
-                      & over (range . start) (untransformPosition transformerParams tx)
-                      & over (range . end) (untransformPosition transformerParams tx)
-
-      return $ fmap transform result
-
+transformServerRsp' STextDocumentDocumentHighlight initialParams result = whenNotebookResult initialParams result $ withTransformer result $ \tx -> do
+  return $ fmap (untransformRanged tx) result
+transformServerRsp' STextDocumentHover initialParams result = whenNotebookResult initialParams result $ withTransformer result $ \tx -> do
+  return (untransformRangedMaybe tx <$> result)
 transformServerRsp' _ _ result = return result
+
+
+untransformRanged :: (HasRange a Range) => HaskellNotebookTransformer -> a -> a
+untransformRanged tx x = x
+  & over (range . start) (untransformPosition transformerParams tx)
+  & over (range . end) (untransformPosition transformerParams tx)
+
+untransformRangedMaybe :: (HasRange a (Maybe Range)) => HaskellNotebookTransformer -> a -> a
+untransformRangedMaybe tx x = x
+  & over (range . _Just . start) (untransformPosition transformerParams tx)
+  & over (range . _Just . end) (untransformPosition transformerParams tx)

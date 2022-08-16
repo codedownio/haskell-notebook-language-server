4dfe247f8c7f966b0a38fe01160f47db61124f33
Tom McLaughlin <tom@codedown.io>
2022-08-16 01:29:36 -0700

    First default.nix

---
+++ default.nix
@@ -0,0 +1,29 @@
+{ mkDerivation, aeson, attoparsec, base, bytestring, containers
+, filepath, ghc, ghc-lib-parser, ghc-parser, ghc-paths, hpack
+, ihaskell, lens, lib, lsp-types, monad-logger, mtl, network-uri
+, optparse-applicative, regex-base, regex-pcre-builtin, safe
+, sandwich, string-interpolate, text, text-rope, unliftio
+, unliftio-core, vector
+}:
+mkDerivation {
+  pname = "haskell-notebook-language-server";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    base containers ghc ghc-lib-parser ghc-parser ghc-paths ihaskell
+    lsp-types regex-base regex-pcre-builtin string-interpolate text
+    vector
+  ];
+  libraryToolDepends = [ hpack ];
+  executableHaskellDepends = [
+    aeson attoparsec base bytestring containers filepath ghc
+    ghc-lib-parser ghc-parser ghc-paths ihaskell lens lsp-types
+    monad-logger mtl network-uri optparse-applicative regex-base
+    regex-pcre-builtin safe sandwich string-interpolate text text-rope
+    unliftio unliftio-core vector
+  ];
+  prePatch = "hpack";
+  license = lib.licenses.bsd3;
+}

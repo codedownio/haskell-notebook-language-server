c5834f1336deca39f32eaf75c131108832739eb1
Tom McLaughlin <tom@codedown.io>
2023-07-03 00:51:15 -0700

    Working on generate.sh

--- generate.sh
+++ generate.sh
@@ -3,4 +3,10 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPTDIR
 
-nix run .#cabal2nix -- . --no-check > default.nix
+nix run .#cabal2nix -- . --no-check --compiler ghc-8.10 > default.nix
+
+# nix run .#cabal2nix -- . --no-check --compiler ghc-8.10 > default-810.nix
+# nix run .#cabal2nix -- . --no-check --compiler ghc-9.0 > default-90.nix
+# nix run .#cabal2nix -- . --no-check --compiler ghc-9.2 > default-92.nix
+# nix run .#cabal2nix -- . --no-check --compiler ghc-9.4 > default-94.nix
+# nix run .#cabal2nix -- . --no-check --compiler ghc-9.6 > default-96.nix

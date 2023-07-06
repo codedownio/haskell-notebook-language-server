94a1ce2e09a4c82fc466b125f1c439adb281d854
Tom McLaughlin <tom@codedown.io>
2023-07-05 18:27:54 -0700

    Try setting --stack-yaml in .dir-locals.el

--- test/.dir-locals.el
+++ test/.dir-locals.el
@@ -3,6 +3,7 @@
   .
   (
    (haskell-process-args-stack-ghci . ("--ghci-options=-ferror-spans"
+                                       "--stack-yaml" "stack-9.2.8.yaml"
                                        "--no-build"
                                        "--no-load"
                                        "haskell-notebook-language-server:lib"

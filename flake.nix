ef7fd7782e2b7958a8f9df561d378c8b4b845229
Tom McLaughlin <tom@codedown.io>
2023-11-29 01:18:52 -0700

    Move big overlay from flake.nix to own file

--- flake.nix
+++ flake.nix
@@ -13,60 +13,7 @@
       let
         overlays = [
           haskellNix.overlay
-          (final: prev: {
-            hixProject = compiler-nix-name: src:
-              final.haskell-nix.hix.project {
-                inherit src;
-                evalSystem = "x86_64-linux";
-                inherit compiler-nix-name;
-                modules = [
-                  {
-                    reinstallableLibGhc = false;
-                    nonReinstallablePkgs = [
-                      "rts"
-                      "ghc-heap"
-                      "ghc-prim"
-                      "integer-gmp"
-                      "integer-simple"
-                      "base"
-                      "deepseq"
-                      "array"
-                      "ghc-boot-th"
-                      "pretty"
-                      "template-haskell"
-                      "ghcjs-prim"
-                      "ghcjs-th"
-                      "ghc-bignum"
-                      "exceptions"
-                      "stm"
-                      "ghc-boot"
-                      "ghc"
-                      "Cabal"
-                      "Win32"
-                      "array"
-                      "binary"
-                      "bytestring"
-                      "containers"
-                      "directory"
-                      "filepath"
-                      "ghc-boot"
-                      "ghc-compact"
-                      "ghc-prim"
-                      "hpc"
-                      "mtl"
-                      "parsec"
-                      "process"
-                      "text"
-                      "time"
-                      "transformers"
-                      "unix"
-                      "xhtml"
-                      "terminfo"
-                    ];
-                  }
-                ];
-              };
-          })
+          (import ./nix/fix-ghc-pkgs-overlay.nix)
         ];
 
         pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
---
+++ nix/fix-ghc-pkgs-overlay.nix
@@ -0,0 +1,54 @@
+final: prev: {
+  hixProject = compiler-nix-name: src:
+    final.haskell-nix.hix.project {
+      inherit src;
+      evalSystem = "x86_64-linux";
+      inherit compiler-nix-name;
+      modules = [
+        {
+          reinstallableLibGhc = false;
+          nonReinstallablePkgs = [
+            "rts"
+            "ghc-heap"
+            "ghc-prim"
+            "integer-gmp"
+            "integer-simple"
+            "base"
+            "deepseq"
+            "array"
+            "ghc-boot-th"
+            "pretty"
+            "template-haskell"
+            "ghcjs-prim"
+            "ghcjs-th"
+            "ghc-bignum"
+            "exceptions"
+            "stm"
+            "ghc-boot"
+            "ghc"
+            "Cabal"
+            "Win32"
+            "array"
+            "binary"
+            "bytestring"
+            "containers"
+            "directory"
+            "filepath"
+            "ghc-boot"
+            "ghc-compact"
+            "ghc-prim"
+            "hpc"
+            "mtl"
+            "parsec"
+            "process"
+            "text"
+            "time"
+            "transformers"
+            "unix"
+            "xhtml"
+            "terminfo"
+          ];
+        }
+      ];
+    };
+}

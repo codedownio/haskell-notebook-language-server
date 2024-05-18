dc0a705324e1813294f2fd64ccf7013ad8b5c446
Tom McLaughlin <tom@codedown.io>
2024-05-18 04:17:46 -0700

    Try getting rid of all transformers

--- src/Language/LSP/Notebook.hs
+++ src/Language/LSP/Notebook.hs
@@ -20,23 +20,23 @@ import Language.LSP.Transformer
 
 
 type HaskellNotebookTransformer =
-  DirectiveToPragma -- Convert :set -XFoo directives to LANGUAGE pragmas
-  :> StripDirective -- Strip all remaining directives
-  :> ExpressionToDeclaration -- Convert naked expressions to declarations
-  :> StatementToDeclaration -- Convert naked statements to declarations
-  :> ImportSifter -- Sift imports to the top
-  :> HeaderTransformer -- Add unsafePerformIO import at top
-  :> PragmaSifter -- Sift pragmas to the top (above imports)
+  -- DirectiveToPragma -- Convert :set -XFoo directives to LANGUAGE pragmas
+  -- :> StripDirective -- Strip all remaining directives
+  -- :> ExpressionToDeclaration -- Convert naked expressions to declarations
+  -- :> StatementToDeclaration -- Convert naked statements to declarations
+  -- :> ImportSifter -- Sift imports to the top
+  HeaderTransformer -- Add unsafePerformIO import at top
+  -- :> PragmaSifter -- Sift pragmas to the top (above imports)
 
 expressionToDeclarationParams :: FilePath -> Params ExpressionToDeclaration
 expressionToDeclarationParams = EDParams 10
 
 transformerParams :: FilePath -> Params HaskellNotebookTransformer
 transformerParams ghcLibDir =
-  DTPParams ghcLibDir
-  :> SDParams ghcLibDir
-  :> expressionToDeclarationParams ghcLibDir
-  :> STDParams ghcLibDir
-  :> ghcLibDir
-  :> ["import System.IO.Unsafe (unsafePerformIO)"]
-  :> ghcLibDir
+  -- DTPParams ghcLibDir
+  -- :> SDParams ghcLibDir
+  -- :> expressionToDeclarationParams ghcLibDir
+  -- :> STDParams ghcLibDir
+  -- :> ghcLibDir
+  ["import System.IO.Unsafe (unsafePerformIO)"]
+  -- :> ghcLibDir

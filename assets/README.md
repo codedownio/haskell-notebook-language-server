b6b509315044699c4d1e1571a7b3d1ec14fe6e2c
Tom McLaughlin <tom@mac1.local>
2024-12-06 19:46:50 -0800

    Able to to dev-root for both platforms

---
+++ assets/README.md
@@ -0,0 +1,2 @@
+
+The libffi.a in this folder was compiled from Nixpkgs master on 12/6/2024 on a x86_64-darwin machine. It's include to support cross-compiling to that target on an aarch64-darwin.
--- flake.nix
+++ flake.nix
@@ -46,11 +46,21 @@
 
             fix_dylib "$out/bin/haskell-notebook-language-server" libiconv.2.dylib libiconv.dylib
             fix_dylib "$out/bin/haskell-notebook-language-server" libffi.8.dylib libffi.dylib
+            fix_dylib "$out/bin/haskell-notebook-language-server" libncursesw.6.dylib libncurses.dylib
             check_no_nix_refs "$out/bin/haskell-notebook-language-server"
           '';
-          packages.haskell-notebook-language-server.components.exes.haskell-notebook-language-server.configureFlags = [
-            ''--ghc-options="-optl-Wl,-dead_strip -optl-Wl,-dead_strip_dylibs -optl-Wl,-force_load,${pkgs.pkgsStatic.libffi}/lib/libffi.a"''
-          ];
+          packages.haskell-notebook-language-server.components.exes.haskell-notebook-language-server.configureFlags = let
+            # Nixpkgs can't currently give us a cross-compiled x86_64-darwin libffi.a when we're building on aarch64-darwin.
+            # So, we bundle one in the repo.
+            # Tried to also detect if we're on aarch64-darwin, so it can work normally if the build machine is x86_64-darwin,
+            # but that is deliberately difficult here (builtins.currentSystem is considered an "impure builtin".)
+            libffi = if pkgs.stdenv.targetPlatform.system == "x86_64-darwin"
+                     then "${./assets/libffi.a}"
+                     else "${pkgs.pkgsStatic.libffi}/lib/libffi.a";
+            in
+              [
+                ''--ghc-options="-optl-Wl,-dead_strip -optl-Wl,-dead_strip_dylibs -optl-Wl,-force_load,${libffi}"''
+              ];
         };
 
         flake = compiler-nix-name: src: (pkgs.hixProject compiler-nix-name src [baseModules]).flake {};

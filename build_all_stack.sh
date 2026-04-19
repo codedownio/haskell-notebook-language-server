7ae201a8be4f3b28dfac2c946d021a6118e09bb2
thomasjm <tom@codedown.io>
2026-04-18 18:36:01 -0700

    ci: more

--- build_all_stack.sh
+++ build_all_stack.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-stack build --stack-yaml stack/stack-8.10.7.yaml
 stack build --stack-yaml stack/stack-9.0.2.yaml
 stack build --stack-yaml stack/stack-9.2.8.yaml
 stack build --stack-yaml stack/stack-9.4.8.yaml
 stack build --stack-yaml stack/stack-9.6.6.yaml
-stack build --stack-yaml stack/stack-9.8.2.yaml
+stack build --stack-yaml stack/stack-9.8.4.yaml
+stack build --stack-yaml stack/stack-9.10.2.yaml
+stack build --stack-yaml stack/stack-9.12.2.yaml
